46a9e509f18c763ad7b2b4049d49a25732d4fc2b
Saranraj <www.saranrajcoder@gmail.com>
2024-07-08 11:37:34 +0530

    Database Connected

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
-app.listen(4000,()=>{
-    console.log('Server Running');
+// MongoDB Connection 
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log('Database Connected')
+}).catch((err) => {
+    console.log(err)
+})
+
+
+app.listen(() => {
+    console.log(`Server Running ${process.env.PORT}`);
 })
