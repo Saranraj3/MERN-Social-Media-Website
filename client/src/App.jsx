6a33a7860a0b00cf79a04d517891dbfde8d4bc43
Saranraj <www.saranrajcoder@gmail.com>
2024-07-08 10:57:58 +0530

    Some Packages Installed Some Minor Changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
+import ProtectedRoute from './Auth/ProtectedRoute';
 
 function App() {
+
+  const [authenticated, setauthenticated] = useState(false);
+
+  const handlelogin = () => {
+    setauthenticated(true);
+  }
+
+  const handlesignup = () => {
+    setauthenticated(true);
+  }
+
   return (
-    <div>App</div>
+    <Routes>
+      <Route path='/' element={<Navigate to='/login' />} />
+      <Route path='/login' element={<Login onLogin={handlelogin} />} />
+      <Route path='/signup' element={<Signup onSignup={handlesignup} />} />
+      <Route path='/home' element={<ProtectedRoute authenticated={authenticated} component={<Home />} />} />
+    </Routes>
   )
 }
 
